0483ed2f4e124202bea88365a56f87a4eddc2d70
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-02-05 22:50:49 -0300

    first commit

---
+++ src/main/java/com/nicolas/product_backend/models/Product.java
@@ -0,0 +1,93 @@
+package com.nicolas.product_backend.models;
+
+import java.security.PublicKey;
+
+public class Product {
+    private int id;
+    private String description;
+    private boolean promotion;
+    private boolean newProduct;
+    private int idCategory;
+
+    public Product(int id, String description, boolean promotion, boolean newProduct, int idCategory, String name,
+            double price) {
+        this.id = id;
+        this.description = description;
+        this.promotion = promotion;
+        this.newProduct = newProduct;
+        this.idCategory = idCategory;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(boolean promotion) {
+        this.promotion = promotion;
+    }
+
+    public boolean isNewProduct() {
+        return newProduct;
+    }
+
+    public void setNewProduct(boolean newProduct) {
+        this.newProduct = newProduct;
+    }
+
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+
+    }
+
+    public Product() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
